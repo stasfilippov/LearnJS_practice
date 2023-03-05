5c9266a70bd1a0e5ca6cf69bd72c21e8d05ffe39
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-03-05 19:17:59 +0300

    add: ИЛИ

--- 2.11 Логические операторы/ ?/script.js
+++ 2.11 Логические операторы/ ?/script.js
@@ -5,8 +5,8 @@ alert( null || 2 || undefined ); // 2
 
 
 //=============================
-
 alert( alert(1) || 2 || alert(3) ); // alert(1)=> undefined и 2
 
-
+//=============================
+alert( 1 && null && 2 ); // null 
 
