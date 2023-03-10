f77ca58e537f2cf3c12b674aa74abda326834e67
stasfilippov <kron888999@gmail.com>
2023-03-10 19:34:31 +0300

    add: задача 3

--- 2.15 Функции/script.js
+++ 2.15 Функции/script.js
@@ -52,17 +52,17 @@ function checkAge(age) {
     }
 } */
 
-function checkAge(age) {
+/* function checkAge(age) {
     return (age > 18) ? true : confirm('Родители разрешили?');
 }
 
 function checkAge(age) {
     return (age > 18) || confirm('Родители разрешили?');
 }
-
+ */
 // ==================== ЗАДАНИЕ №3
 //Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
-min(2, 5) == 2
+/* min(2, 5) == 2
 min(3, -1) == -1
 min(1, 1) == 1
 
@@ -74,4 +74,26 @@ function getMinNumb (a, b) {
     }
 }
 
-console.log(getMinNumb(1, 1));
+console.log(getMinNumb(1, 1)); */
+
+// ==================== ЗАДАНИЕ №3
+//Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
+//Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
+
+/* pow(3, 2) = 3 * 3 = 9
+pow(3, 3) = 3 * 3 * 3 = 27
+pow(1, 100) = 1 * 1 * ...* 1 = 1 */
+
+let x = prompt ('Введите число'); 
+let n = prompt ('Введите степень'); 
+
+function pow (x, n) {
+    if (n >= 1) {
+        let result = x ** n;
+        return result; 
+    } else {
+        alert ('Введите целое число для степени');
+    }
+}
+
+console.log(pow (x, n)); 
