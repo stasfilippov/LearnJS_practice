d132196442679fcf00f1265a70b216cff5b04fc1
stasfilippov <kron888999@gmail.com>
2023-05-12 20:55:26 +0300

    add: задача 2

--- Работа с DOM/Атрибуты и свойства/index.js
+++ Работа с DOM/Атрибуты и свойства/index.js
@@ -1,5 +1,28 @@
-//Напишите код для выбора элемента с атрибутом data-widget-name из документа и прочитайте его значение.
+//1. Напишите код для выбора элемента с атрибутом data-widget-name из документа и прочитайте его значение.
 
-const elem = document.querySelector('[data-widget-name]')
+// const elem = document.querySelector('[data-widget-name]')
 
-console.log(elem.dataset.widgetName);
+// console.log(elem.dataset.widgetName);
+
+//2. Сделайте все внешние ссылки оранжевыми, изменяя их свойство style.
+
+// добавление стиля для одной ссылки
+let links = document.querySelectorAll('a');
+
+for (let link of links) {
+    const href = link.getAttribute('href');
+
+    if(!href) continue;
+
+    if (!href.includes('://')) continue;
+
+    if (href.startsWith('http://internal.com')) continue;
+
+    link.style.color = 'orange';
+}
+
+//Другой вариант проверки 
+// let selector = 'a[href*="://"]:not([href^="http://internal.com"])';
+// let links = document.querySelectorAll(selector);
+
+// links.forEach(link => link.style.color = 'orange');
