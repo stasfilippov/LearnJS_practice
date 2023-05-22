2167731ea36176356d3c00bb24eb6028f5354fba
stasfilippov <kron888999@gmail.com>
2023-05-22 14:41:54 +0300

    add: создание дерева

--- Документ (DOM)/Изменение документа/index.js
+++ Документ (DOM)/Изменение документа/index.js
@@ -5,7 +5,8 @@
 // elem.innerHTML = text
 // elem.textContent = text // +
 
-//2. Создайте функцию clear(elem), которая удаляет всё содержимое из elem.
+//2. =====================================
+//Создайте функцию clear(elem), которая удаляет всё содержимое из elem.
 // const elem = document.querySelector('#elem');
 
 // function clear (elem) { 
@@ -14,26 +15,76 @@
 
 // clear(elem); // очищает список
 
-//3. 
+//3. ===================================
 // alert(table); // 
 
 // table.remove();
 
-//4. 
+//4. ==================================
+// const ul = document.createElement('ul');
 
-const ul = document.createElement('ul');
+// document.body.append(ul);
 
-document.body.append(ul);
+// while (true) {
+//     let data = prompt('Введите значение', '')
 
-while (true) {
-    let data = prompt('Введите значение', '')
+//     if (!data) {
+//         break;
+//     }
 
-    if (!data) {
-        break;
+//     let li = document.createElement('li');
+//     li.textContent = data;
+//     ul.append(li);
+// }
+
+//5 ===============================================
+
+let data = {
+    "Рыбы": {
+        "форель": {},
+        "лосось": {}
+    },
+
+    "Деревья": {
+        "Огромные": {
+        "секвойя": {},
+        "дуб": {}
+        },
+        "Цветковые": {
+        "яблоня": {},
+        "магнолия": {}
+        }
+    }
+};
+
+
+function createTree(container, obj) {
+    container.append(createTreeDom(obj));
+}
+
+function createTreeDom(obj) {
+    // если нет дочерних элементов, то вызов возвращает undefined
+    // и элемент <ul> не будет создан
+    if (!Object.keys(obj).length) return;
+
+    let ul = document.createElement('ul');
+
+    for (let key in obj) {
+        let li = document.createElement('li');
+        li.innerHTML = key;
+
+        let childrenUl = createTreeDom(obj[key]);
+        if (childrenUl) {
+            li.append(childrenUl);
+        }
+
+        ul.append(li);
     }
 
-    let li = document.createElement('li');
-    li.textContent = data;
-    ul.append(li);
+    return ul;
 }
+
+let container = document.getElementById('container');
+createTree(container, data);
+
 
